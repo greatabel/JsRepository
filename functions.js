e5e8a86c35fe7d490be9bf774655dc48cb93aa0d
greatabel <myreceiver2for2github@gmail.com>
2014-12-28 03:03:42 +0800

    update function

--- functions.js
+++ functions.js
@@ -1,2 +1,24 @@
 
-var showStr_functions = "function test";
+var showStr_functions = "ch2:function test ";
+
+var result = add(5, 5);
+
+function add(num1, num2) {
+            return num1 + num2;
+}
+
+showStr_functions+=('result='+result);
+
+ var add = function(num1, num2) {
+            return num1 + num2;
+};
+
+showStr_functions+=(' add(1,20)='+add(1,20))
+
+//错误的方式
+// var resultA = addA(5, 5);
+//         var addA = function(num1, num2) {
+//             return num1 + num2;
+// };
+
+
