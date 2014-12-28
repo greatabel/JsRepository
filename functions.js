7f22d7c72c0b788722d6f770ac85437830dfc838
greatabel <myreceiver2for2github@gmail.com>
2014-12-28 12:16:26 +0800

    update function

--- functions.js
+++ functions.js
@@ -28,4 +28,22 @@ sayHi(); // outputs "Hi!"
 var sayHi2 = sayHi;
 sayHi2();       // outp
 
+//等效真正发生的是：
+ var sayHi = new Function("console.log(\"Hi1!\");");
+   sayHi();        // outputs "Hi!"
+   var sayHi2 = sayHi;
+   sayHi2();       // outputs "Hi!"
 
+
+var numbers = [ 1, 5, 8, 4, 7, 10, 2, 6 ]; 
+numbers.sort(function(first, second) {
+       return first - second;
+   });
+console.log(numbers); // "[1, 2, 4, 5, 6, 7, 8, 10]" 
+numbers.sort();
+console.log(numbers);       
+console.log('way is 10 after 1,see http://stackoverflow.com/questions/10198257/comparing-2-strings-alphabetically-for-sorting-purposes');
+
+
+
+console.log('%c ch2 is end ---','color:red');
