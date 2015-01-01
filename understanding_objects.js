a3d582abbc415e6433764caa77131e64123c4774
greatabel <myreceiver2for2github@gmail.com>
2015-01-01 16:23:43 +0800

    alter

--- understanding_objects.js
+++ understanding_objects.js
@@ -32,3 +32,75 @@ console.log(person1.hasOwnProperty("toString"));
 delete person1.name;
 console.log('after delete:'+("name" in person1));
 console.log(person1.name);
+
+console.log('\n');
+
+var property;
+        for (property in person2) {
+            console.log("Name: " + property);
+            console.log("Value: " + person2[property]);
+}
+console.log('%c a list of an objects\'s property','color:pink');
+var object = person2;
+var properties = Object.keys(object); // if you want to mimic for-in behavior
+var i, len;
+        for (i=0, len=properties.length; i < len; i++){
+            console.log("Name: " + properties[i]);
+            console.log("Value: " + object[properties[i]]);
+}
+
+
+var person1 = {
+_name: "Nicholas",
+get name() { console.log("Reading name");
+                return this._name;
+            },
+set name(value) {
+console.log("Setting name to %s", value);
+        this._name = value;
+    }
+};
+console.log(person1.name);
+person1.name = "Greg";
+console.log(person1.name);
+
+var person1 = {
+ name: "Nicholas"
+};
+Object.defineProperty(person1, "name", { 
+ enumerable: false
+   });
+   console.log("name" in person1);
+console.log(person1.propertyIsEnumerable("name")); 
+var properties = Object.keys(person1);
+   console.log(properties.length);
+Object.defineProperty(person1, "name", { 
+ configurable: false
+});
+   // try to delete the Property
+delete person1.name; 
+console.log("name" in person1);
+   console.log(person1.name);
+ //following will be error
+// Object.defineProperty(person1, "name", 
+// 	{ configurable: true
+// });
+
+   var person1 = {
+            name: "Nicholas"
+};
+//等价于：
+var person1 = {};
+        Object.defineProperty(person1, "name", {
+            value: "Nicholas",
+            enumerable: true,
+            configurable: true,
+            writable: true
+        });
+
+
+
+
+
+
+
