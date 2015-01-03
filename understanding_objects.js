3cf24f97d9a90fa0c2be42b0212f63125093107d
greatabel <myreceiver2for2github@gmail.com>
2015-01-03 23:14:53 +0800

    alter

--- understanding_objects.js
+++ understanding_objects.js
@@ -181,7 +181,18 @@ console.log(descriptor.configurable);
 console.log(descriptor.writable);
 console.log(descriptor.value);
 
+console.log('Preventing object modification');
+var person1 = {
+       name: "Nicholas"
+};
+console.log(Object.isExtensible(person1));
+Object.preventExtensions(person1); 
+console.log(Object.isExtensible(person1));
+person1.sayName = function() { 
+	console.log(this.name);
 
+};
+console.log("sayName" in person1);
 
 
 
