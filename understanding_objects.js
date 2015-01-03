56903bde1ca1ecbd04477a7d59ce933d7170970a
greatabel <myreceiver2for2github@gmail.com>
2015-01-03 23:10:00 +0800

    alter

--- understanding_objects.js
+++ understanding_objects.js
@@ -144,6 +144,45 @@ console.log("name" in person1);
 person1.name = "Greg";
 console.log(person1.name);
 
+//Defining Multiple Properties
+var person1 = {};
+Object.defineProperties(person1, {
+
+ _name: {
+           value: "Nicholas",
+           enumerable: true,
+           configurable: true,
+           writable: true
+},
+// accessor property 
+name: {
+           get: function() {
+               console.log("Reading name");
+               return this._name;
+           },
+           set: function(value) {
+               console.log("Setting name to %s", value);
+               this._name = value;
+           },
+           enumerable: true,
+           configurable: true
+       }
+});
+
+console.log(person1.name);
+
+//Retrieving Property Attributes
+ var person1 = {
+       name: "Nicholas"
+   };
+var descriptor = Object.getOwnPropertyDescriptor(person1, "name");
+console.log(descriptor.enumerable);
+console.log(descriptor.configurable);
+console.log(descriptor.writable);
+console.log(descriptor.value);
+
+
+
 
 
 
