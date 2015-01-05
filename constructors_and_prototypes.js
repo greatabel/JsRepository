5da0fd966a30587dfc50276ca6d0f9e8556352d7
greatabel <myreceiver2for2github@gmail.com>
2015-01-05 11:54:33 +0800

    update

--- constructors_and_prototypes.js
+++ constructors_and_prototypes.js
@@ -75,3 +75,64 @@ function hasPrototypeProperty(Object,name){
 console.log(hasPrototypeProperty(book,"title"));
 console.log(hasPrototypeProperty(book,"hasOwnProperty"));
 
+console.log('Object.getPrototypeOf() --->');
+var object = {};
+var prototype = Object.getPrototypeOf(object);
+
+console.log(prototype === Object.prototype);
+console.log(object.__proto__);
+console.log('to see one object is a prototype for another:');
+console.log(Object.prototype.isPrototypeOf(object));
+
+
+var object = {}; 
+console.log(object.toString());
+   object.toString = function() {
+       return "[object Custom]";
+}; 
+console.log(object.toString());
+  delete object.toString;
+console.log(object.toString()); // "[object Object]"
+
+// no effect - delete only works on own properties
+delete object.toString;
+console.log(object.toString());
+
+console.log('--Using Prototypes with Constructors');
+function PersonP(name){
+	this.name = name;
+}
+
+PersonP.prototype.sayName = function(){
+	console.log('prototype sayName:'+this.name);
+}
+
+var person11 = new PersonP("bill");
+var person22 = new PersonP("steve");
+
+person11.sayName();
+person22.sayName();
+
+PersonP.prototype.favorites = [];
+person11.favorites.push('egg');
+person22.favorites.push('bread');
+
+console.log(person11.favorites);
+console.log(person22.favorites);
+
+//more clear way to add properties to prototype
+PersonP.prototype = {
+	sayHello: function(){
+		console.log('sayHello:'+this.name);
+	},
+	toString: function(){
+		return "[Person "+ this.name+"]";
+	}
+};
+var person33 = new PersonP("tesla");
+person33.sayHello();
+console.log(person33.toString());
+
+
+
+
