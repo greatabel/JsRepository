66313130940f635c095a570e6831d0dada003391
greatabel <myreceiver2for2github@gmail.com>
2016-03-17 01:08:32 +0800

    multiple listener to emitter

---
+++ Node.js By Example/ch1Node.js Fundamentals /10working with events/EventEmitter_multiple_listener.js
@@ -0,0 +1,15 @@
+//  引入 events 模块
+var events = require('events');
+
+// 创建 eventEmitter 对象
+var emitter = new events.EventEmitter();
+
+emitter.on('someEvent', function(arg1, arg2) {
+    console.log('listener1', arg1, arg2);
+});
+
+emitter.on('someEvent', function(arg1, arg2) {
+    console.log('listener2', arg1, arg2);
+});
+
+emitter.emit('someEvent','arg1 参数', 'arg2 参数');
