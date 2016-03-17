ec46a09374c7bd1c9125e921202521267b2ab9cd
greatabel <myreceiver2for2github@gmail.com>
2016-03-17 15:00:01 +0800

    util

---
+++ Node.js By Example/ch1Node.js Fundamentals /10working with events/util_isError4.js
@@ -0,0 +1,7 @@
+var util = require('util');
+
+console.log(util.isError(new Error()))
+ // true
+console.log(util.isError(new TypeError()))
+  // true
+console.log(util.isError({ name: 'Error', message: 'an error occurred' }))
