57a63c86ba7526075e269c87d2290b2e61f5da36
greatabel <myreceiver2for2github@gmail.com>
2016-03-18 23:40:12 +0800

    server layer

---
+++ Node.js By Example/ch2Architecting the Project/1Introducing the basic layers of the application/server.js
@@ -0,0 +1,9 @@
+var http = require('http')
+
+var fs = require('fs')
+
+http.createServer(function(req,res){
+
+}).listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/')
