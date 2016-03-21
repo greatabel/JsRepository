3181cfe21ac771477a6d434f1b4b77954588358c
greatabel <myreceiver2for2github@gmail.com>
2016-03-21 23:46:50 +0800

    grunt build

--- Node.js By Example/ch2Architecting the Project/2task runner and building system/Gruntfile.js
+++ Node.js By Example/ch2Architecting the Project/2task runner and building system/Gruntfile.js
@@ -1,14 +1,27 @@
-module.experts = function(grunt) {
-    grunt.initConfig(
-            {
-                concat: {
-                    javascript: {
-                        src: 'src/**/*.js',
-                        dest: 'build/script.js'
-                    }
-                }
-            }
-        );
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.registerTask('default',['concat']);
+module.exports = function(grunt) {
+	grunt.initConfig({
+		concat: {
+			javascript: {
+				src: 'src/**/*.js',
+				dest: 'build/scripts.js'
+			}
+		},
+		uglify: {
+			javascript: {
+				files: {
+					'build/scripts.min.js': '<%= concat.javascript.dest %>'
+				}
+			}
+		},
+		watch: {
+			javascript: {
+				files: ['<%= concat.javascript.src %>'],
+				tasks: ['concat', 'uglify']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', ['concat', 'uglify', 'watch']);
 }
