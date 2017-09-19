dc4ab8a350c0f600b4740ed43531528f84a854c7
greatabel <myreceiver2for2github@gmail.com>
2017-09-19 16:11:55 +0800

    toggle

--- js by example/1build a todo list/scripts.js
+++ js by example/1build a todo list/scripts.js
@@ -1,9 +1,60 @@
+// class ToDoClass {
+//   constructor() {
+//     alert('Hello World!');
+//   }
+// }
 class ToDoClass {
+
   constructor() {
-    alert('Hello World!');
+    this.tasks = [
+        {task: 'Go to Dentist', isComplete: false},
+         {task: 'Do Gardening', isComplete: true},
+         {task: 'Renew Library Account', isComplete: false},
+    ];
+    this.loadTasks();
   }
+
+  loadTasks() {
+    let tasksHtml = this.tasks.reduce((html, task, index) => html += this.generateTaskHtml(task, index), '');
+  document.getElementById('taskList').innerHTML = tasksHtml;
+
+  }
+
+  generateTaskHtml(task, index) {
+     return `
+      <li class="list-group-item checkbox">
+       <div class="row">
+        <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
+         <label><input id="toggleTaskStatus" type="checkbox"  
+         onchange="toDo.toggleTaskStatus(${index})" value="" class="" 
+         ${task.isComplete?'checked':''}></label>
+        </div>
+        <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text ${task.isComplete?'complete':''}">
+         ${task.task}
+       </div>
+       <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area">
+         <a class="" href="/" onClick="toDo.deleteTask(event, ${index})"><i 
+         id="deleteTask" data-id="${index}" class="delete-icon glyphicon 
+         glyphicon-trash"></i></a>
+        </div>
+       </div>
+      </li>
+    `;
+    }
+
+     toggleTaskStatus(index) {
+      this.tasks[index].isComplete = !this.tasks[index].isComplete;
+       this.loadTasks();
+     }
+     
+     deleteTask(event, taskIndex) {
+       event.preventDefault();
+       this.tasks.splice(taskIndex, 1);
+       this.loadTasks();
+     }
 }
 
+let toDo;
 window.addEventListener("load", function() {
-  var toDo = new ToDoClass();
+  toDo = new ToDoClass();
 });
