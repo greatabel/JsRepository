ebd49d57129db43c18bd6c497189bd468e6590b7
greatabel <myreceiver2for2github@gmail.com>
2018-06-22 16:39:08 +0800

    01vue-components

---
+++ Learning Vue.js 2/ch3Components/02learn-vue/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }} | {{user}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Learning Vue.js',
+      user: 'abel'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #f2b98f;
+}
+</style>
