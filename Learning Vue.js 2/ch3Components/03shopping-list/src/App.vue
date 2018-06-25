64a1b3c8843522375cd7363b2158e7785204720a
greatabel <myreceiver2for2github@gmail.com>
2018-06-25 16:05:08 +0800

    03shopping-list

---
+++ Learning Vue.js 2/ch3Components/03shopping-list/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="container">
+    <img src="./assets/logo.png">
+
+    <add-item-component></add-item-component>
+  </div>
+</template>
+
+<script>
+import AddItemComponent from './components/AddItemComponent'
+
+export default {
+  // name: 'App',
+  components: {
+    AddItemComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
