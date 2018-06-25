127952b482b57b662d3fe71bbe03b4a00732e1f8
greatabel <myreceiver2for2github@gmail.com>
2018-06-25 17:32:10 +0800

    ItemsComponent.vue

---
+++ Learning Vue.js 2/ch3Components/03shopping-list/src/components/ItemsComponent.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <item-component v-for="item in items" :item="item"> </item-component>
+</div>
+</template>
+
+<script>
+ import ItemComponent from './ItemComponent'
+
+ export default {
+    components: {
+        ItemComponent
+    },
+    propos: ['items']
+ }
+</script>
+
+<style scoped>
+</style>
