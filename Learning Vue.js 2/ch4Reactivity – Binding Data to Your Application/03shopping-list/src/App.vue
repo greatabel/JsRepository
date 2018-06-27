a0272a4763923ba28c571c1fcddf473c4d6bcf9c
greatabel <myreceiver2for2github@gmail.com>
2018-06-27 11:58:29 +0800

    directive

---
+++ Learning Vue.js 2/ch4Reactivity – Binding Data to Your Application/03shopping-list/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app" class="container">
+    <h2>{{ title }}</h2>
+    <input type="text" v-model="title" >
+    <add-item-component @add="add"></add-item-component>
+    <items-component :items="items"></items-component>
+    <div class="footer">
+      <hr/>
+      <change-title-component></change-title-component>
+    </div>
+  </div>
+</template>
+
+<script>
+import AddItemComponent from './components/AddItemComponent'
+import ItemsComponent from './components/ItemsComponent'
+import ChangeTitleComponent from './components/ChangeTitleComponent'
+
+export default {
+  // name: 'App',
+  components: {
+      AddItemComponent,
+      ItemsComponent,
+      ChangeTitleComponent
+  },
+  data() {
+    return {
+      items: [{ text: 'Bananas', checked: true }, { text: 'Apples', checked: false }],
+      title: 'shopping-list',
+
+    }
+  },
+  methods: {
+    add (newItem) {
+      alert('add ' + newItem)
+    }
+  }
+
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Learning Vue.js 2/ch4Reactivity – Binding Data to Your Application/03shopping-list/src/components/AddItemComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="input-group">
+      <input v-model="newItem"  type="text" class="input form-control" placeholder="add shopping list item">
+      <span class="input-group-btn">
+        <button  @click="addItem" class="btn btn-default" type="button">Add!</button>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+  
+export default {
+    data () {
+      return {
+        newItem: ''
+      }
+    },
+    methods: {
+      addItem () {
+        var text
+
+        text = this.newItem.trim()
+        if (text) {
+          console.log('in additem' + this.newItem);
+          this.$emit('add', this.newItem)
+          this.newItem = ''
+        }
+      }
+    }
+}
+</script>
+
+<style scoped></style>
