c8aa065eff753e198d2a866b086d0b661fc913e0
greatabel <myreceiver2for2github@gmail.com>
2018-06-27 11:39:23 +0800

    add Vue.filter

--- Learning Vue.js 2/ch4Reactivity – Binding Data to Your Application/02pomodoro/src/main.js
+++ Learning Vue.js 2/ch4Reactivity – Binding Data to Your Application/02pomodoro/src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App'
 
+Vue.filter('lowercase', (key) => {
+    return key.toLowerCase()
+})
+
+Vue.filter('uppercase', (key) => {
+    return key.toUpperCase()
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: 'app',
