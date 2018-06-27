e77a771de5935bad425d308e5309f64779c83f9c
greatabel <myreceiver2for2github@gmail.com>
2018-06-27 11:44:45 +0800

    leftpad filter

--- Learning Vue.js 2/ch4Reactivity – Binding Data to Your Application/02pomodoro/src/main.js
+++ Learning Vue.js 2/ch4Reactivity – Binding Data to Your Application/02pomodoro/src/main.js
@@ -9,6 +9,13 @@ Vue.filter('uppercase', (key) => {
     return key.toUpperCase()
 })
 
+Vue.filter('leftpad', (value) => {
+    if (value >= 10) {
+        return value
+    }
+    return '0' + value
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: 'app',
