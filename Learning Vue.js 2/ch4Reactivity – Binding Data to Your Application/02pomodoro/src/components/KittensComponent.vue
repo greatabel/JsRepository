8764cfa9b8eb7c328bd80b74e4db9fb07b4ca260
greatabel <myreceiver2for2github@gmail.com>
2018-06-27 12:07:14 +0800

    KittensComponent.vue v-bind:src

--- Learning Vue.js 2/ch4Reactivity – Binding Data to Your Application/02pomodoro/src/components/KittensComponent.vue
+++ Learning Vue.js 2/ch4Reactivity – Binding Data to Your Application/02pomodoro/src/components/KittensComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="well">
-    <!-- <img :src="catImgSrc" /> -->
+    <img v-bind:src="catImgSrc" />
   </div>
 </template>
 
@@ -8,4 +8,11 @@
 </style>
 
 <script>
+export default {
+ data () {
+    return {
+        catImgSrc: "http://thecatapi.com/api/images/get?size=med"
+    }
+ }
+}
 </script>
--- Learning Vue.js 2/ch4Reactivity – Binding Data to Your Application/03shopping-list/src/App.vue
+++ Learning Vue.js 2/ch4Reactivity – Binding Data to Your Application/03shopping-list/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="container">
     <h2>{{ title }}</h2>
-    <input type="text" v-model="title" >
+    实验: <input type="text" v-model="title" >
     <add-item-component @add="add"></add-item-component>
     <items-component :items="items"></items-component>
     <div class="footer">
