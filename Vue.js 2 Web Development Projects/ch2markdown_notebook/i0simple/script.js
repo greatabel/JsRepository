2a87567f12d4bdd44f570e4b930dffc6626dc20f
greatabel <myreceiver2for2github@gmail.com>
2018-07-11 22:08:56 +0800

    show html

--- Vue.js 2 Web Development Projects/ch2markdown_notebook/i0simple/script.js
+++ Vue.js 2 Web Development Projects/ch2markdown_notebook/i0simple/script.js
@@ -2,8 +2,15 @@ new Vue({
     el: '#notebook',
     data () {
         return {
-            content: 'This is a note.'
+            content: 'This is a **note** ! \
+            <span style="color:red">test color</span>'
         }
-    }
+    },
+
+    computed: {
+        notePreview () {
+            return marked(this.content)
+        },
+    },
 
 })
