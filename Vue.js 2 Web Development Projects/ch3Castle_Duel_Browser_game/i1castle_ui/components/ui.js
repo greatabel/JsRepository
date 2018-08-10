793d97219217e642bf1d805e5579bb975f86daeb
greatabel <myreceiver2for2github@gmail.com>
2018-08-10 11:17:07 +0800

    turn ui

--- Vue.js 2 Web Development Projects/ch3Castle_Duel_Browser_game/i1castle_ui/components/ui.js
+++ Vue.js 2 Web Development Projects/ch3Castle_Duel_Browser_game/i1castle_ui/components/ui.js
@@ -1,8 +1,15 @@
 Vue.component('top-bar', {
     template: `<div class="top-bar">
                 <div class="player p0"> {{ players[0].name }} </div>
+
+                <div class="turn-counter">
+                  <img class="arrow" src="svg/turn.svg" />
+                  <div class="turn"> {{ turn }} 轮</div>
+                </div>
+
                 <div class="player p1"> {{ players[1].name }} </div>
                </div>`,
+
     props: ['players', 'currentPlayerIndex', 'turn'],
     created() {
         console.log(this.players)
--- Vue.js 2 Web Development Projects/ch3Castle_Duel_Browser_game/i1castle_ui/main.js
+++ Vue.js 2 Web Development Projects/ch3Castle_Duel_Browser_game/i1castle_ui/main.js
@@ -3,9 +3,9 @@ new Vue({
     el: '#app',
 
     data: state,
-    template: `<div id="#pp">\
-                <top-bar :turn="turn" :current-player-index="currentPlayerIndex"\
-                         :players="players"/> \
+    template: `<div id="#pp">
+                <top-bar :turn="turn" :current-player-index="currentPlayerIndex"
+                         :players="players"/>
                </div>`,
 
 })
