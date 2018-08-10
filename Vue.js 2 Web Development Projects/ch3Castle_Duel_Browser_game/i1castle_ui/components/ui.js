95c5f69183c9bc9aaad0c17ef98fb294c92b441d
greatabel <myreceiver2for2github@gmail.com>
2018-08-10 10:55:49 +0800

    remove :Fallback font will be used while loading

--- Vue.js 2 Web Development Projects/ch3Castle_Duel_Browser_game/i1castle_ui/components/ui.js
+++ Vue.js 2 Web Development Projects/ch3Castle_Duel_Browser_game/i1castle_ui/components/ui.js
@@ -1,7 +1,8 @@
 Vue.component('top-bar', {
-    template: '<div class="top-bar">\
-                顶栏\
-               </div>',
+    template: `<div class="top-bar">
+                <div class="player p0"> {{ players[0].name }} </div>
+                <div class="player p1"> {{ players[1].name }} </div>
+               </div>`,
     props: ['players', 'currentPlayerIndex', 'turn'],
     created() {
         console.log(this.players)
--- Vue.js 2 Web Development Projects/ch3Castle_Duel_Browser_game/i1castle_ui/main.js
+++ Vue.js 2 Web Development Projects/ch3Castle_Duel_Browser_game/i1castle_ui/main.js
@@ -3,10 +3,10 @@ new Vue({
     el: '#app',
 
     data: state,
-    template: '<div id="#pp">\
+    template: `<div id="#pp">\
                 <top-bar :turn="turn" :current-player-index="currentPlayerIndex"\
                          :players="players"/> \
-               </div>',
+               </div>`,
 
 })
 
