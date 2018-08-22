027ff0bf6e1c7160683dcb26ec4dce352a3abaa6
greatabel <myreceiver2for2github@gmail.com>
2018-08-22 22:23:24 +0800

    ul list

--- Vue.js 2 Web Development Projects/ch4advanced_project_setup/demo/src/main.js
+++ Vue.js 2 Web Development Projects/ch4advanced_project_setup/demo/src/main.js
@@ -3,5 +3,12 @@ import Vue from 'vue'
 
 new Vue({
   el: '#app',
-  render: h => h('div', 'hello world'),
+  // render: h => h('div', 'hello world'),
+    render (h) {
+    return h('ul', { 'class': 'movies' }, [
+                    h('li', { 'class': 'movie' }, 'Star Wars'),
+                    h('li', { 'class': 'movie' }, 'Blade Runner'),
+                  ])
+    }
+
 })
