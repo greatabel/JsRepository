99c986cefd300952212ecfc7f456d2ba72088015
greatabel <myreceiver2for2github@gmail.com>
2018-08-23 16:24:10 +0800

    router

--- Vue.js 2 Web Development Projects/ch5support_center/support-center/src/main.js
+++ Vue.js 2 Web Development Projects/ch5support_center/support-center/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import 'babel-polyfill'
+import AppLayout from './components/AppLayout.vue'
+import router from './router'
 
 
 new Vue({
   el: '#app',
-  render: h => h('div', '支持中心'),
+  render: h => h(AppLayout),
+  router
 })
