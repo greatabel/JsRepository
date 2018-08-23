e258e3d9f917e5358f04effd0c8624e4c7561206
greatabel <myreceiver2for2github@gmail.com>
2018-08-23 16:53:08 +0800

    exact

--- Vue.js 2 Web Development Projects/ch5support_center/support-center/src/components/NavMenu.vue
+++ Vue.js 2 Web Development Projects/ch5support_center/support-center/src/components/NavMenu.vue
@@ -1,6 +1,15 @@
 <template>
     <nav class="menu">
-        <router-link :to="{ name: 'home'}" >主页 </router-link>
+        <router-link :to="{ name: 'home'}" exact>主页 </router-link>
         <router-link :to="{ name: 'faq'}" >FAQ </router-link>
     </nav>
 </template>
+
+<style lang="stylus" scoped>
+@import '../style/imports';
+
+.router-link-active {
+    border-bottom-color: $primary-color;
+}
+
+</style>
