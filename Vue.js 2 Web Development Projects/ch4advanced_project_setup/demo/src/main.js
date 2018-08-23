c8ca8f6f7b817cd728bd84f5b486d7299bdfb76d
greatabel <myreceiver2for2github@gmail.com>
2018-08-23 13:04:24 +0800

    sass

--- Vue.js 2 Web Development Projects/ch4advanced_project_setup/demo/src/main.js
+++ Vue.js 2 Web Development Projects/ch4advanced_project_setup/demo/src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import 'babel-polyfill'
 import Test from './Test.vue'
 import TestA from './TestA.vue'
+import TestB from './TestB.vue'
 
 
 new Vue({
   el: '#app',
   ...Test,
   ...TestA,
+  ...TestB,
   // render: h => h('div', 'hello world'),
     // render (h) {
     // return h('ul', { 'class': 'movies' }, [
