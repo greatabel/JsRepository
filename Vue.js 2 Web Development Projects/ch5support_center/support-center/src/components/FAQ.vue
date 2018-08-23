a0c3c7471aca56e9ddd3d1c5163247ce62fc9b52
greatabel <myreceiver2for2github@gmail.com>
2018-08-23 17:04:13 +0800

    fetch api

--- Vue.js 2 Web Development Projects/ch5support_center/support-center/src/components/FAQ.vue
+++ Vue.js 2 Web Development Projects/ch5support_center/support-center/src/components/FAQ.vue
@@ -1,5 +1,42 @@
 <template>
    <main class="faq">
      <h1>经常被问的问题</h1>
+
+     <div class="error" v-if="error">
+        加载问题失败
+     </div>
+
+     <section class="list">
+         <article v-for="question of questions">
+             <h2 v-html="question.title"></h2>
+             <p v-html="question.content"></p>
+         </article>
+     </section>
+
    </main>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+        questions: [],
+        error: null,
+    }
+  },
+  created () {
+    fetch('http://localhost:3000/questions').then(response => {
+        if(response.ok) {
+            return response.json()
+        } else {
+            return Promise.reject('error')
+        }
+    }).then(result => {
+        this.questions = result
+    }).catch(e => {
+        this.error = e
+    })
+
+  },
+}
+</script>
