99bff43eac7848be3fba404c74b87eaaa0418318
greatabel <myreceiver2for2github@gmail.com>
2018-08-23 11:40:00 +0800

    Test.vue

---
+++ Vue.js 2 Web Development Projects/ch4advanced_project_setup/demo/src/Test.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p> {{ message }} </p>
+        <input v-model="message" />
+  <!--   </div>
+
+    <div> -->
+        <ul class="movies">
+       <li v-for="movie of movies" class="movie">
+         {{ movie.title }}
+        </li> 
+        </ul>
+    </div>
+
+</template>
+
+<script>
+export default {
+     data () {
+       return {
+         message: 'hello world!',
+         movies: [
+           { title: 'Star Wars' },
+           { title: 'Blade Runner' },
+],
+} },
+} 
+</script>
+
+<style>
+p {
+    color: grey;
+}
+</style>
--- Vue.js 2 Web Development Projects/ch4advanced_project_setup/demo/src/main.js
+++ Vue.js 2 Web Development Projects/ch4advanced_project_setup/demo/src/main.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import 'babel-polyfill'
+import Test from './Test.vue'
 
 
 new Vue({
   el: '#app',
+  ...Test,
   // render: h => h('div', 'hello world'),
-    render (h) {
-    return h('ul', { 'class': 'movies' }, [
-                    h('li', { 'class': 'movie' }, 'Star Wars'),
-                    h('li', { 'class': 'movie' }, 'Blade Runner'),
-                  ])
-    }
+    // render (h) {
+    // return h('ul', { 'class': 'movies' }, [
+    //                 h('li', { 'class': 'movie' }, 'Star Wars'),
+    //                 h('li', { 'class': 'movie' }, 'Blade Runner'),
+    //               ])
+    // }
 
 })
