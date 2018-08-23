6d6aea2cfe6cd05580c03600d4f35acc65c110d1
greatabel <myreceiver2for2github@gmail.com>
2018-08-23 13:05:30 +0800

    less

--- Vue.js 2 Web Development Projects/ch4advanced_project_setup/demo/src/TestB.vue
+++ Vue.js 2 Web Development Projects/ch4advanced_project_setup/demo/src/TestB.vue
@@ -4,8 +4,15 @@
      </article>
 </template>
 
-<style lang="sass" scoped>
+<!-- <style lang="sass" scoped>
  .article
    .title
      border-bottom: solid 3px rgba(red, .2)
+</style> -->
+
+<style lang="less" scoped>
+    .article {
+       .title {
+         border-bottom: solid 3px fade(blue, 20%);
+} }
 </style>
