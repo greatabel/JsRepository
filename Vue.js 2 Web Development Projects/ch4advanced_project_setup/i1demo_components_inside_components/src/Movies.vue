0719b29b691fdf37b932e8019347072c5cb42cc2
greatabel <myreceiver2for2github@gmail.com>
2018-08-23 13:30:21 +0800

    i1demo_components_inside_components

---
+++ Vue.js 2 Web Development Projects/ch4advanced_project_setup/i1demo_components_inside_components/src/Movies.vue
@@ -0,0 +1,25 @@
+<template>
+<ul class="movies">
+     <Movie v-for="movie of movies" 
+        :key="movie.id"
+        :movie="movie"/>
+      
+</ul>
+</template>
+
+<script>
+import Movie from './Movie.vue'
+
+export default {
+    components: {
+        Movie,
+    },
+  data () {
+         return {
+           movies: [
+             { id: 0, title: 'Star Wars II' },
+             { id: 1, title: 'Blade Runner II' },
+            ],  }
+    }, 
+}
+</script>
--- Vue.js 2 Web Development Projects/ch4advanced_project_setup/i1demo_components_inside_components/src/main.js
+++ Vue.js 2 Web Development Projects/ch4advanced_project_setup/i1demo_components_inside_components/src/main.js
@@ -1,21 +1,11 @@
 import Vue from 'vue'
 import 'babel-polyfill'
-import Test from './Test.vue'
-import TestA from './TestA.vue'
-import TestB from './TestB.vue'
+import Movies from './Movies.vue'
 
 
 new Vue({
   el: '#app',
-  ...Test,
-  ...TestA,
-  ...TestB,
-  // render: h => h('div', 'hello world'),
-    // render (h) {
-    // return h('ul', { 'class': 'movies' }, [
-    //                 h('li', { 'class': 'movie' }, 'Star Wars'),
-    //                 h('li', { 'class': 'movie' }, 'Blade Runner'),
-    //               ])
-    // }
+  ...Movies,
+
 
 })
