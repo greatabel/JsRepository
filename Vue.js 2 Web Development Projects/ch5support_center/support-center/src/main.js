e30f74118d16b8f1fb9be3533c781b6d2312b80a
greatabel <myreceiver2for2github@gmail.com>
2018-08-27 16:21:35 +0800

    fetch.js

--- Vue.js 2 Web Development Projects/ch5support_center/support-center/src/main.js
+++ Vue.js 2 Web Development Projects/ch5support_center/support-center/src/main.js
@@ -3,6 +3,9 @@ import 'babel-polyfill'
 import AppLayout from './components/AppLayout.vue'
 import router from './router'
 import './global-components'
+import VueFetch from './plugins/fetch'
+
+Vue.use(VueFetch)
 
 new Vue({
   el: '#app',
