b47bc0fa2c693669b5e058a796b8f15f7ecd20a8
greatabel <myreceiver2for2github@gmail.com>
2018-08-29 23:31:48 +0800

    state

--- Vue.js 2 Web Development Projects/ch5support_center/support-center/src/main.js
+++ Vue.js 2 Web Development Projects/ch5support_center/support-center/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import 'babel-polyfill'
 import AppLayout from './components/AppLayout.vue'
 import router from './router'
+import state from './state'
 import './global-components'
 import VueFetch from './plugins/fetch'
+
 
 Vue.use(VueFetch, {
     baseUrl: 'http://localhost:3000/',
@@ -11,6 +13,7 @@ Vue.use(VueFetch, {
 
 new Vue({
   el: '#app',
+  data: state,
   render: h => h(AppLayout),
   router
 })
