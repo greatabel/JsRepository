26fd7d349d44175b5e585ba42fab80c9046f9313
greatabel <myreceiver2for2github@gmail.com>
2018-08-30 15:35:51 +0800

    change fetch to accept parameter

--- Vue.js 2 Web Development Projects/ch5support_center/support-center/src/plugins/fetch.js
+++ Vue.js 2 Web Development Projects/ch5support_center/support-center/src/plugins/fetch.js
@@ -10,13 +10,22 @@ export default {
     },
 }
 
-export async function $fetch (url) {
-    const response = await fetch(`${baseUrl}${url}`)
+export async function $fetch (url, options) {
+    const finalOptions = Object.assign({}, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    }, options)
+
+    const response = await fetch(`${baseUrl}${url}`, finalOptions)
     if (response.ok) {
         const data = await response.json()
         return data
     } else {
-        const error = new Error('error')
+        const message = await response.text() 
+        const error = new Error(message) 
+        error.response = response
         throw error
     }
 
