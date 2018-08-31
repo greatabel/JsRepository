ca29496593c142825ce02157b0cb3468322ed509
greatabel <myreceiver2for2github@gmail.com>
2018-08-31 17:13:23 +0800

    styles

--- Vue.js 2 Web Development Projects/ch6Geolocated_Blog/geoblog/src/plugins/fetch.js
+++ Vue.js 2 Web Development Projects/ch6Geolocated_Blog/geoblog/src/plugins/fetch.js
@@ -1,5 +1,4 @@
-import state from '../state'
-import router from '../router'
+// import router from '../router'
 
 let baseUrl
 
@@ -26,17 +25,7 @@ export async function $fetch (url, options) {
         const data = await response.json()
         return data
     } else if (response.status === 403) {
-    // If the session is no longer valid
-    // We logout
-    state.user = null
-
-    // If the route is private
-    // We go to the login screen
-    if (router.currentRoute.matched.some(r => r.meta.private)) {
-      router.replace({ name: 'login', params: {
-        wantedRoute: router.currentRoute.fullPath,
-      }})
-    }
+    
   } else {
         const message = await response.text() 
         const error = new Error(message) 
