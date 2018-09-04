7e506cb49dd80044005622201ceca3175bf75e7a
greatabel <myreceiver2for2github@gmail.com>
2018-09-04 15:48:03 +0800

    handleMessage

--- Vue.js 2 Web Development Projects/ch6Geolocated_Blog/geoblog/src/components/Login.vue
+++ Vue.js 2 Web Development Projects/ch6Geolocated_Blog/geoblog/src/components/Login.vue
@@ -11,8 +11,22 @@
    
 <script>
 export default {
-methods: { openGoogleSignin () {
-// TODO 
-},
-}, }
+    methods: { 
+        openGoogleSignin () {
+            // TODO 
+            const url = 'http://localhost:3000/auth/google'
+            const name = 'google_login'
+            const specs = 'width=500, height=500'
+            window.open(url, name, specs)
+        },
+        handleMessage({data, origin}){
+            if (origin !== 'http://localhost:3000') {
+                return 
+            }
+            if (data === 'success') {
+                this.login()
+            }
+        },
+    }, 
+}
 </script>
