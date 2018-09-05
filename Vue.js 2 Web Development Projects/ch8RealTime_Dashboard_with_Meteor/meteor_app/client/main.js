de6e36e909060655379b28066cdb234735c500fc
greatabel <myreceiver2for2github@gmail.com>
2018-09-05 17:01:52 +0800

    server

--- Vue.js 2 Web Development Projects/ch8RealTime_Dashboard_with_Meteor/meteor_app/client/main.js
+++ Vue.js 2 Web Development Projects/ch8RealTime_Dashboard_with_Meteor/meteor_app/client/main.js
@@ -4,8 +4,13 @@ import App from './components/App.vue'
 import router from './router'
 
 import VueMeteorTracker from 'vue-meteor-tracker'
+import 'vue-progress-path/dist/vue-progress-path.css'
 
 Vue.use(VueMeteorTracker)
+
+Vue.use(VueProgress, {
+  defaultShape: 'semicircle',
+})
 
 
 Meteor.startup(() => {
